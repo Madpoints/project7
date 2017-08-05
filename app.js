233bfeece9d7e28d0e4ce2af154243c53901fc29
John Furtado <jf8846@gmail.com>
2017-08-05 20:56:23 +0000

    user can get a random article

--- app.js
+++ app.js
@@ -1,28 +1,61 @@
 /*global $*/
 
-var handlers = {
-    getWikiTitles: function() {
+function getWikiTitles() {
         var query = $('#searchInput').val();
         
-        $.ajax({
-        url: 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrsearch=' + query + "&callback=?",
-        contentType: "application/json; charset=utf-8",
-        async: false,
-        dataType: "jsonp",
-        success: function (response) {
-            console.log(response.query.pages);
-            
-            var sources = response.query.pages;
-            var html = "<ul class='form-control' id='sourceOption'>";
-            $.each(sources, function(index, source){
-                html += "<li value='"+ source.pageid + "'>" + source.title + "</li>"
-            })
-            html += "</ul>";
-            $('#results').html(html)
+        if (query === '') {
+            return;
         }
+        
+        $.ajax({
+            url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=" + query + "&callback=?",
+            contentType: "application/json; charset=utf-8",
+            async: false,
+            dataType: "jsonp",
+            success: function(response) {
+                var sources = response.query.pages;
+                var html = "<ul id='sourceOption'class='list-group'>";
+                
+                $.each(sources, function(index, source){
+                    html += "<li class='list-group-item' value='"+ source.pageid + "'><b><a href='https://en.wikipedia.org/wiki/" + source.title  + "'>" + source.title + "</a></b><p>" + source.extract + "<p></li>";
+                });
+                
+                html += "</ul>";
+                $('#results').html(html);
+            }
         }); 
         
         $('#searchInput').val("");
-    }
-};
+}
 
+function getRandomWikiArticle() {
+    $.ajax({
+        url: "https://en.wikipedia.org/w/api.php?format=json&action=query&generator=random&grnnamespace=0&prop=revisions|images&rvprop=content&grnlimit=1",
+        contentType: "application/json; charset=utf-8",
+        async: false,
+        dataType: "jsonp",
+        success: function(response) {
+            var randomArticle = response.query.pages;
+            
+            $.each(randomArticle, function(index, randomArticle) {
+                window.location.href = "https://en.wikipedia.org/wiki/" + randomArticle.title;    
+            });
+        }
+    });
+}
+    
+
+
+$(document).ready(function() {
+    
+    $('#wikiSearch').on("click", function(event) {
+        event.preventDefault();
+        getWikiTitles();
+    });
+    
+    $('#wikiRandom').on("click", function(event) {
+        event.preventDefault();
+        getRandomWikiArticle();
+    });
+    
+});
